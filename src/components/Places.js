3a85d80df27661dbb6251d15346f562529a06032
Nick <nick.italiano@mapbox.com>
2017-12-04 14:37:53 -0800

    Initial commit

---
+++ src/components/Places.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { PixelRatio, Platform } from 'react-native';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
+
+const styles = MapboxGL.StyleSheet.create({
+  icon: {
+    iconAllowOverlap: true,
+    iconSize: Platform.OS === 'android' ? 0.50 : 0.25,
+  },
+});
+
+class Places extends React.Component {
+  static SelectedSymbolID = 'store-locator-selected-symbol';
+  static UnselectedSymbolID = 'store-locator-places-unselected-symbols';
+
+  static propTypes = {
+    /**
+     * FeatureCollection of points that we want to appear on the map.
+     */
+    featureCollection: PropTypes.object.isRequired,
+
+    /**
+     * Active feature index
+     */
+    activeIndex: PropTypes.number,
+
+    /**
+     * Override any default styles on the inactive marker layer
+     */
+    style: PropTypes.any,
+
+    /**
+     * Override any default styles on the active marker layer
+     */
+    activeStyle: PropTypes.any,
+  };
+
+  static defaultProps = {
+    activeIndex: 0,
+  };
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      activeIndex: props.activeIndex,
+      activeID: props.activeID,
+    };
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.state.activeIndex !== nextProps.activeIndex) {
+      this.setState({
+        activeIndex: nextProps.activeIndex,
+        activeID: this.props.featureCollection.features[nextProps.activeIndex].id,
+      });
+    }
+  }
+
+  render () {
+    if (!this.props.featureCollection) {
+      return null;
+    }
+    return (
+      <MapboxGL.ShapeSource id='store-locator-places-source' shape={this.props.featureCollection}>
+        <MapboxGL.SymbolLayer
+          id={Places.UnselectedSymbolID}
+          filter={['!=', '$id', this.state.activeID]}
+          style={[styles.icon, this.props.style]} />
+
+        <MapboxGL.SymbolLayer
+          id={Places.SelectedSymbolID}
+          aboveLayerID={Places.UnselectedSymbolID}
+          filter={['==', '$id', this.state.activeID]}
+          style={[styles.icon, this.props.activeStyle]} />
+      </MapboxGL.ShapeSource>
+    )
+  }
+}
+
+export default Places;
